6b0c29001198433ffcbbc1988b9dda921c1d67ea
root <root@bjhw-sys-rpm0221.bjhw.baidu.com>
2025-01-16 16:54:28 +0800

    use new llvm

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
-LLVM_REPO=3rdparty/llvm-project
+git submodule update --init --recursive
+
+# llvm 只需要编译一次
+
+# mkdir third_party/llvm-project/build
+
+# pushd third_party/llvm-project/build
+# cmake -G "Unix Makefiles" \
+#   -DCMAKE_BUILD_TYPE=Release \
+#   -DLLVM_ENABLE_PROJECTS="mlir" \
+#   -DCMAKE_INSTALL_PREFIX=/path/to/install/llvm \
+#   ../llvm
+
+# make -j$(nproc)
+
+popd
+
+LLVM_REPO=third_party/llvm-project
 BUILD_DIR=build
 
 rm -rf $BUILD_DIR
